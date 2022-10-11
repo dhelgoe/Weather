0ea7c561eb8f219ed079709896724e4b854b38f0
derek helgoe <derek1896@gmail.com>
2022-10-10 19:19:42 -0500

    textcontext

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 var city = document.querySelector('#submit')
+var cityname = document.querySelector('#cityname')
+var currenttemp = document.querySelector('#currenttemp')
+var windspeed = document.querySelector('#windspeed')
+var humidity = document.querySelector('#humidity')
+var photo = document.querySelector('#icon')
+
 
 city.addEventListener('click', function(){
     var userInput = document.querySelector('#citySearch')
@@ -9,6 +15,9 @@ city.addEventListener('click', function(){
         var lat = data[0].lat
         var lon = data[0].lon
         console.log(lat,lon)
+        console.log(data[0].name)
+        cityname.textContent=data[0].name
+
 
     fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=d98d42ce70a6969152ff956c930079d8`).then((data) => {
         return data.json()
@@ -18,6 +27,16 @@ city.addEventListener('click', function(){
         console.log(data.list[0].main.temp)
         var temperature = data.list[0].weather[0];
         console.log(temperature)
+        currenttemp.textContent=data.list[0].main.temp
+        console.log(data.list[0].wind.deg)
+        console.log(data.list[0].wind.speed)
+        windspeed.textContent=data.list[0].wind.speed
+        humidity.textContent=data.list[0].main.humidity
+        console.log(data.list[0].weather[0].icon)
+        var icon = data.list[0].weather[0].icon
+        var iconurl="http://openweathermap.org/img/wn/"+icon+"@2x.png"
+        console.log(iconurl)
+        photo.src=iconurl
 
     } ) 
         
